4a108e14bdbe3aa65ec24cf27575f7b85201a8dc
Mohammad Ahsan Khan <ahsan@Ahsans-Mac-mini.local>
2013-05-12 10:23:44 -0700

    Partial implementation of Epoll based UDP server

---
+++ pubsub/cpp/udp-epoll/udpepollserver.cpp
@@ -0,0 +1,177 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <assert.h>
+#include "framework.h"
+
+// Main event loop
+class UdpEpollMain: public EventMain {
+protected:
+
+    int listener;
+    int dest;
+    EventHandler *server;
+    EventHandler *client;
+    bool loopEnd;
+
+public:
+
+    void initialize() {
+        loopEnd = false;
+        dest = listener = -1;
+
+    }
+
+    struct MyContext {
+        int fd;
+        sockaddr_in dest;
+    }
+    struct MyEventData {
+        int fd;
+        EventHandler *pHandler;
+    };
+#define MAXEVENTS 64
+
+    void process() {
+        int efd;
+        epoll_event event = {0};
+        epoll_event events[MAXEVENTS];
+
+        efd = epoll_create1(0);
+
+        if (efd == -1) {
+            perror("epoll_create");
+            exit(1);
+        }
+        if (this->listener != -1) {
+            MyEventData data = {listener, NULL};
+            event.data.ptr = new MyEventData(data);
+            event.events = EPOLLIN;
+            if (epoll_ctl(efd, EPOLL_CTL_ADD, listener, &event) == -1) {
+                perror("epoll_ctl");
+                exit(1);
+            }
+        }
+        if (this->dest != -1) {
+            MyEventData data = {dest, client};
+            event.data.ptr = new MyEventData(data);
+            event.events = EPOLLIN;
+            if (epoll_ctl(efd, EPOLL_CTL_ADD, dest, &event) == -1) {
+                perror("epoll_ctl");
+                exit(1);
+            }
+        }
+
+        while (!loopEnd) {
+            int nevents = epoll_wait(efd, events, MAXEVENTS, -1);
+            for (int i=0; i < nevents; i++) {
+                epoll_event *pev = &events[i];
+                MyEventData *data = (MyEventData*)pev->data.ptr;
+                if ((pev->events & EPOLLERR) ||
+                       (pev->events & EPOLLHUP) ||
+                       !(pev->events & EPOLLIN)) {
+                    fprintf(stderr, "epoll error\n");
+                    close(data->fd);
+                    delete data;
+                    continue;
+                }
+                INFO_OUT("Reading socket %d", i);
+                char buf[1024];
+                struct sockaddr_in si_from;
+                int slen = sizeof(si_from);
+                ssize_t  result = recvfrom(data->fd, buf, sizeof(buf), 0,
+                                           &si_from, &slen );
+                if (result < 0) {
+                    perror("recv");
+                    close(data->fd);
+                    delete data;
+                    continue;
+                } else if (result == 0) {
+                    close(data->fd);
+                    delete data;
+                    break;
+                }
+                if (data->pHandler) {
+                    MyContext fromcontext = {data->fd, si_from};
+                    data->pHandler->setContext((Context*) &fromcontext);
+                    data->pHandler->process(buf, result, true);
+                }
+
+            }
+        }
+        close(listener);
+    }
+
+    void cancelLoop() {
+        loopEnd = true;
+    }
+
+    void bindServer(const char *port, EventHandler *pProcessor) {
+        struct sockaddr_in sin = { 0 };
+
+        sin.sin_family = AF_INET;
+        sin.sin_addr.s_addr = 0;
+        sin.sin_port = htons(atoi(port));
+
+        listener = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        this->server = pProcessor;
+        setParent(pProcessor);
+        fcntl(listener, F_SETFL, O_NONBLOCK);
+        int oneval = 1;
+        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &oneval, sizeof(oneval));
+        if (bind(listener, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
+            perror("bind");
+            return;
+        }
+        INFO_OUT("Bound to port %s", port);
+    }
+
+    void send(EventHandler *p, char *data, int len, bool isDataEnd) {
+        MyContext *pContext;
+        if (!p | !(pContext =p->getContext())) {
+            INFO_OUT("Invalid context");
+            return;
+        }
+        
+        if (::sendto((pContext->fd, data, len, &pContext->dest,
+                      sizeof(pContext->dest)) == -1) {
+            perror("send");
+        }
+        INFO_OUT("Done sending");
+
+    }
+    
+    void connectToServer(const char *address, const char *port,
+            EventHandler *pProcessor) {
+        sockaddr_in sin = { 0 };
+        MyContext pContext = new MyContext;
+
+        sin.sin_family = AF_INET;
+        sin.sin_port = htons(atoi(port));
+        inet_pton(AF_INET, address, &(sin.sin_addr));
+        sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        pContext->dest = sin;
+        pContext->fd = sockfd;
+        this->client = pProcessor;
+        setParent(pProcessor);
+        pProcessor->setContext((Context*) (void*) pContext);
+        fcntl(dest, F_SETFL, O_NONBLOCK);
+        pProcessor->enable();
+
+    }
+
+};
+
+
+#ifdef BUILDTEST
+UdpEpollMain epollMain;
+EventMain *g_pmainProcessor = &epollMain;
+#endif
