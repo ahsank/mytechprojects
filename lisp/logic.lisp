0c2a10455428b68c8b18ffb6db435315ae02f470
Mohammad Khan <ahsank@ahsank-macbookair.roam.corp.google.com>
2014-07-11 19:04:22 -0700

    prolog

--- lisp/logic.lisp
+++ lisp/logic.lisp
@@ -112,8 +112,79 @@ taking recursively bound variables into account."
 
 ;; Clauses are on the predicate's property list
 ;; 
-(defun get-clasues (pred) (get pred 'clauses))
+(defun get-clauses (pred) (get pred 'clauses))
 (defun predicate (relation) (first relation))
 (defvar *db-predicates* nil
   "A list of all predicates stored in the database.")
 ;; I could have just taken code from http://norvig.com/paip?
+
+;; (<- (member ?item (?x . ?rest)) (member ?item ?rest))
+;; Here predicate is member
+;; Prolog: member(X, [Y|L]) :- member(X, L).
+(defmacro <- (&rest clause)
+  "Add a clause to the data base."
+  `(add-clause ',clause))
+
+(defun add-clause (clause)
+  "Add a clause to the data base, indexed by head's predicate."
+  ;; The predicate must be a non-variable symbol.
+  (let ((pred (predicate (clause-head clause))))
+    (assert (and (symbolp pred) (not (variable-p pred))))
+    (pushnew pred *db-predicates*)
+    (setf (get pred 'clauses)
+	  (nconc (get-clauses pred) (list clause)))
+    pred))
+
+(defun clear-db ()
+  "Remove all clauses (for all predicates) from the data base."
+  (mapc #'clear-predicate *db-predicates*))
+
+(defun clear-predicate (predicate)
+  "Remove the clauses for a single predicat."
+  (setf (get predicate 'clauses) nil))
+
+(defun prove (goal bindings)
+  "Return a list of possible solutions to goal."
+  (mapcan #'(lambda (clause)
+	      (let ((new-clause (rename-variables clause)))
+		(prove-all (clause-body new-clause)
+			   (unify goal (clause-head new-clause) bindings))))
+	  (get-clauses (predicate goal))))
+
+(defun prove-all (goals bindings)
+  "Return a list of solutions to the conjunction of goals."
+  (cond ((eq bindings fail) fail)
+	((null goals) (list bindings))
+	(t (mapcan #'(lambda (goal-solution)
+		       (prove-all (rest goals) goal-solution))
+		   (prove (first goals) bindings)))))
+
+(defun rename-variables (x)
+  "Replace all variables in x with new ones."
+  (sublis (mapcar #'(lambda (var) (cons var (gensym (string var))))
+		  (variables-in x))
+	  x))
+
+(defun variables-in (exp)
+  "Return a list of all the variables in EXP."
+  (unique-find-anywhere-if #'variable-p exp))
+
+(defun unique-find-anywhere-if (predicate tree
+					  &optional found-so-far)
+  "Return a list of leaves of tree satisfying predicate,
+with duplication removed."
+  (if (atom tree)
+      (if (funcall predicate tree)
+	  (adjoin tree found-so-far)
+	found-so-far)
+    (unique-find-anywhere-if
+     predicate
+     (first tree)
+     (unique-find-anywhere-if  predicate (rest tree)
+			      found-so-far))))
+
+(defmacro ?- (&rest goals) `(prove-all ',goals no-bindings))
+
+
+
+    
