e2e1c8c620a242d6b56e5808011097ba5c826f4a
ahsank <ahsankhanmail@gmail.com>
2016-02-19 09:53:20 -0800

    Create the api

--- nodejs/testapi/routes/index.js
+++ nodejs/testapi/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+function getFullDigits(number) {
+    return (number < 10 ? "0" : "") + number;
+}
+function getTime() {
+
+    var date = new Date();
+
+    var hour = getFullDigits(date.getHours());
+    var min  = getFullDigits(date.getMinutes())
+    var sec  = getFullDigits(date.getSeconds());
+    return hour + ":" + min + ":" + sec;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    console.log(getTime());
+    res.json({});
+});
+
+
+router.get('/:id', function(req, res, next) {
+    console.log(getTime () + " " + req.params.id);
+    res.json({});
+
 });
 
 module.exports = router;
